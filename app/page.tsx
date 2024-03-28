8009c3692c37d3479f48bce7d8576283a1a01fc3
salva-dali2997 <64043261+salva-dali2997@users.noreply.github.com>
2024-03-28 00:10:07 -0400

    removed if for change

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const Page: React.FC = () => {
         }
     }
     const onAddNewResourceClick = async () => {
-
+        
         const response = await fetch(`/api`, { method: 'POST', body: JSON.stringify(newResource) });//POST
         if (!response.ok) {
             console.error(response.json())
@@ -47,15 +47,11 @@ const Page: React.FC = () => {
     }
 
     const onResourceTitleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        if (e.target.value) {
-            setNewResource({ ...newResource, title: e.target.value })
-        }
+        setNewResource({ ...newResource, title: e.target.value })
     }
 
     const onResourceUrlChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        if (e.target.value) {
-            setNewResource({ ...newResource, url: e.target.value })
-        }
+        setNewResource({ ...newResource, url: e.target.value })
     }
     const onDeleteClick = async (id: any) => {
         const response = await fetch(`/api/${id}`, { method: 'DELETE' });//DELETE
